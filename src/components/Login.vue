50a4dbed52d9ca8964d1f225eb694494ec85a37d
zhangshuai <zhangshuai@yunzhangfang.com>
2021-07-14 10:52:41 +0800

    像后端传参成功

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -3,18 +3,56 @@
     <div id="content">
       <a>欢迎登录</a>
       <br>
-      <input type="text" class="srl" placeholder="请输入帐号">
+      <input type="text" class="srl" placeholder="请输入帐号" :rules="formRules" v-model="from_sub.username">
       <br>
-      <input type="text" class="srl" placeholder="请输入密码">
+      <input type="text" class="srl" placeholder="请输入密码" :rules="formRules" v-model="from_sub.password">
       <br>
-      <button type="submit" class="srl">登录</button>
+      <input type="submit" @click="onsubmit()" class="srl" value="登录">
     </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
-  name: 'Login'
+  name: 'Login',
+  data () {
+    return {
+      from_sub: {
+        'username': '',
+        'password': ''
+      },
+      formRules: {
+        username: [
+          {required: true, message: '请输入用户名', trigger: 'blur'},
+          {min: 2, max: 10, message: '长度在 2 到 10 个字符', trigger: 'blur'}
+        ],
+        password: [
+          {required: true, message: '请输入密码', trigger: 'blur'},
+          {min: 2, max: 10, message: '长度在 2 到 10 个字符', trigger: 'blur'}
+        ]
+      }
+    }
+  },
+  methods: {
+    onsubmit () {
+      var formData = JSON.stringify(this.from_sub)
+      // console.log(this.from_sub)
+      console.log(111, formData)
+      axios({
+        method: 'post',
+        url: 'http://127.0.0.1:5000/login',
+        headers: {
+          'Content-Type': 'text/json'
+        },
+        withCredentials: true,
+        data: formData
+      }).then((res) => {
+        alert(res.data)
+      })
+    }
+  }
 }
 </script>
 
@@ -22,10 +60,11 @@ export default {
 #content {
   height: 500px;
   width: 700px;
-  background-color: #42b983;
+  background-color: antiquewhite;
   margin: 200px auto auto auto;
 }
-.srl{
+
+.srl {
   width: 200px;
   height: 30px;
   margin: auto auto 10px auto;
--- src/main.js
+++ src/main.js
@@ -3,14 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import '@/assets/css/resrt.css'
+// import '@/assets/css/resrt.css'
+// import ElementPlus from 'element-plus'
+// import 'element-plus/lib/theme-chalk/index.css'
 
+// Vue.use(ElementPlus)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {App},
   template: '<App/>'
 })
